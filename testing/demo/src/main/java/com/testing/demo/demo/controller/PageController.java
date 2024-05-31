3ee7e664f4fab8b6ba10a46b745780db72602cca
watermelon0725 <peipei930725@gmail.com>
2024-05-31 15:01:32 +0800

    form表單元素

--- testing/demo/src/main/java/com/testing/demo/demo/controller/PageController.java
+++ testing/demo/src/main/java/com/testing/demo/demo/controller/PageController.java
@@ -3,43 +3,35 @@ package com.testing.demo.demo.controller;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-
-@RestController
+@Controller
 public class PageController {
 
-    private static final Logger logger = LoggerFactory.getLogger(PageController.class);
+    @GetMapping("/form")
+    public ModelAndView showForm() {
+        return new ModelAndView("formPage");
+    }
 
     @PostMapping("/test/form")
-    public ModelAndView testForm(@RequestParam("text") String text) {
+    public ModelAndView testForm(@RequestParam("text") String text, @RequestParam("text2") String text2) {
         // 創建一個 Map 來存儲表單數據
         Map<String, String> data = new HashMap<>();
         // 將表單提交的文本數據存入 Map 中
         data.put("text", text);
-        String text2="input2";
         data.put("text2", text2);
-        // 通過 ModelAndView 綁定數據到視圖中並返回視圖名稱 "form"
-        return new ModelAndView("form", data);
+
+        // 通過 ModelAndView 綁定數據到視圖中並返回視圖名稱 "formPage"
+        return new ModelAndView("formPage", data);
     }
 
     @RequestMapping("hello")
-    public String Sayhi() {
+    public String sayHi() {
         return "Hi";
     }
-    
-    
-
-
-    // @GetMapping("/")   //當訪問跟目錄(/)時，會調用Home()
-    // public String home() {
-    //     logger.info("Home page accessed");
-    //     return "Home"; // 確保在模板目錄中有一個名為Home的視圖
-    // }
 }
