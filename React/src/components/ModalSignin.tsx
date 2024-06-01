039145cd8296de9b65fb605b8566997163a395c8
yieie <ison632413672@gmail.com>
2024-06-01 20:57:46 +0800

    ModalSignin/up

---
+++ React/src/components/ModalSignin.tsx
@@ -0,0 +1,67 @@
+import React, { ReactNode ,useState } from "react";
+import '../components/Modal.css'
+//import useModal from "./useModal";
+
+interface ModalType {
+  children?: ReactNode;
+  isOpen: boolean;
+  toggle: () => void;
+}
+
+export default function Modal(props: ModalType) {
+    const [passwd, setpasswdValue]=useState('')
+    const [account, setaccountValue]=useState('')
+    
+    const handleclick=(event)=>{
+      event.preventDefault()
+      const student={account,passwd}
+      console.log(student)
+      fetch("http://localhost:8080/api/login",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(student)
+      }).then(()=>{
+        console.log("New account added")
+      })
+    }
+
+  return (
+    <>
+        <div className="modal-overlay" >
+          <div  className="modal-box">
+            {props.children}
+            <p className="h2">共同轉帳</p>
+            <div className="form-floating mb-3">
+                <input 
+                value={account}
+                type="text"
+                className="form-control"
+                id="floatingInput"
+                placeholder="name@example.com"
+                onChange={(event)=>setaccountValue(event.target.value)}
+                />
+                <label htmlFor="floatingInput">帳號</label>
+            </div>
+            <div className="form-floating mb-3">
+                <input 
+                value={passwd}
+                type="password" 
+                className="form-control" 
+                id="floatingPassword" 
+                placeholder="Password"
+                onChange={(event)=>setpasswdValue(event.target.value)}
+                />
+                <label htmlFor="floatingPassword">密碼</label>
+            </div>
+            <div className="button">
+                <button type="button" className="btn btn-outline-success" onClick={handleclick}>登入</button> 
+                <hr/>
+            </div>
+            <p className="Signup">還沒有帳號嗎？點此<button type="button" className="btn btn-link" >註冊帳號</button></p>
+          </div>
+        </div>
+        
+        
+    </>
+  );
+}
---
+++ React/src/components/ModalSignup.tsx
@@ -0,0 +1,82 @@
+import React, { ReactNode ,useState } from "react";
+import '../components/ModalSignup.css'
+//import useModal from "./useModal";
+
+interface ModalType {
+  children?: ReactNode;
+  isOpen: boolean;
+  toggle: () => void;
+}
+//Signup 跟 Signin 還沒連動
+export default function Modal(props: ModalType) {
+    const [passwd, setpasswdValue]=useState('')
+    const [account, setaccountValue]=useState('')
+    const [firstname, setfirstnameValue]=useState('')
+    const [lastname, setlastnameValue]=useState('')
+
+    const handleclick=(event)=>{
+      event.preventDefault()
+      const student={firstname,lastname,account,passwd}
+      console.log(student)
+      fetch("http://localhost:8080/api/login",{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify(student)
+      }).then(()=>{
+        console.log("New account added")
+      })
+    }
+
+  return (
+    <>
+        <div className="modal-overlay" >
+          <div  className="modal-box">
+            {props.children}
+            <p className="h2">共同轉帳</p>
+            <form className="row g-3">
+              <div className="col-md-6">
+                  <label htmlFor="inputFirstName4" className="form-label">姓</label>
+                  <input 
+                  value={firstname}
+                  type="account" 
+                  className="form-control"
+                  onChange={(event)=>setfirstnameValue(event.target.value)}
+                  />
+              </div>
+              <div className="col-md-6">
+                <label htmlFor="inputLastName4" className="form-label">名</label>
+                <input 
+                value={lastname}
+                type="account" 
+                className="form-control"
+                onChange={(event)=>setlastnameValue(event.target.value)}
+                />
+              </div>
+              <div className="md-6">
+                <label htmlFor="inputEmail4" className="form-label">帳號</label>
+                <input 
+                value={account}
+                type="account" 
+                className="form-control"
+                onChange={(event)=>setaccountValue(event.target.value)}
+                />
+              </div>
+              <div className="md-6">
+                <label htmlFor="inputPassword4" className="form-label">密碼</label>
+                <input 
+                value={passwd}
+                type="password" 
+                className="form-control" 
+                id="inputPassword4"
+                onChange={(event)=>setpasswdValue(event.target.value)}
+                />
+              </div>
+              <div className="col-12">
+                <button type="submit" className="btn btn-primary" onClick={handleclick}>註冊</button>
+              </div>
+            </form>
+          </div>
+        </div>
+    </>
+  );
+}
