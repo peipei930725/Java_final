1f6c13091d8f97c704b7c7b1ea1a2861475c8235
SiroKu_ <56777898+SiroKu1006@users.noreply.github.com>
2024-06-01 17:49:46 +0800

    chore: Add user login validation in LoginController

--- testing/demo/src/main/java/com/testing/demo/demo/controller/LoginController.java
+++ testing/demo/src/main/java/com/testing/demo/demo/controller/LoginController.java
@@ -1,16 +1,21 @@
 package com.testing.demo.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.testing.demo.demo.model.LoginRequest;
+import com.testing.demo.demo.model.UserCase;
+import com.testing.demo.demo.repository.MyDataRepository;
+
 
 @RestController
 @RequestMapping("/api")
 public class LoginController {
-
+    @Autowired
+    private MyDataRepository myDataRepository;
     @PostMapping("/login")
     public String login(@RequestBody LoginRequest loginRequest) {
         // 打印接收到的账户和密码
@@ -19,8 +24,20 @@ public class LoginController {
 
         // 您可以在这里添加对账户和密码的验证逻辑
         // ...
+        UserCase  existingUserCase = myDataRepository.findByUserAccount(loginRequest.getAccount());
+        if (existingUserCase == null ) {
+            System.out.println("Account is not exist" );
+            return "Login unsuccessful for account: Account is not exist";
+        }else{
+            //驗證登入
+            if (existingUserCase.getUserPassword().equals(loginRequest.getPasswd())) {
+                System.out.println("Password is correct" );
+                return "Login successful for account: " + loginRequest.getAccount();
+            }else{
+                System.out.println("Password is incorrect" );
+                return "Login unsuccessful for account: Password is incorrect";
+            }
 
-        // 返回响应
-        return "Login successful for account: " + loginRequest.getAccount();
+        }
     }
 }
